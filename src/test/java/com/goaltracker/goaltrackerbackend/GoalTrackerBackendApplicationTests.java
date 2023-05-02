13ec7daf64aaabb4affc0c2844b815f1a33967a5
BiggestM <muhamed.mehmedovic@stu.ssst.edu.ba>
2023-05-02 20:50:20 +0200

    commit

---
+++ src/test/java/com/goaltracker/goaltrackerbackend/GoalTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goaltracker.goaltrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoalTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
